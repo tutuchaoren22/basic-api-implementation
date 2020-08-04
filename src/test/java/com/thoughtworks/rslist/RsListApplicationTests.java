149e4f6df38689a5eb681da0afa802d93aa32dd5
duyuqian <809009017@qq.com>
2020-08-04 19:31:46 +0800

    [duyuqian] feat: add the RsEvent class and the getOneRsEvent test

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,35 @@
 package com.thoughtworks.rslist;
 
+import com.thoughtworks.rslist.api.RsEvent;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RsListApplicationTests {
+
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void shouldGetOneRsEvent() throws Exception {
+        mockMvc.perform(get("/rs/list/1"))
+                .andExpect(jsonPath("$.eventName",is("第一条事件")))
+                .andExpect(jsonPath("$.keyword",is("分类一")))
+                .andExpect(status().isOk());
+    }
+
 
 }
