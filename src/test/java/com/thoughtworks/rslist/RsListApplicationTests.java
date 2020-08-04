0bf932cb662ea09abaf93094d35624edcb47d701
duyuqian <809009017@qq.com>
2020-08-04 19:57:56 +0800

    [duyuqian] feat: add the addRsEvent test

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -59,6 +61,24 @@ class RsListApplicationTests {
                 .andExpect(jsonPath("$[1].keyword", is("分类二")))
                 .andExpect(jsonPath("$[2].eventName", is("第三条事件")))
                 .andExpect(jsonPath("$[2].keyword", is("分类三")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldAddRsEventGivenEventNametAndKeyword() throws Exception {
+        String requestJson = "{\"eventName\":\"第四条事件\",\"keyword\":\"分类四\"}";
+        mockMvc.perform(post("/rs/add").content(requestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
+                .andExpect(jsonPath("$[0].keyword", is("分类一")))
+                .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
+                .andExpect(jsonPath("$[1].keyword", is("分类二")))
+                .andExpect(jsonPath("$[2].eventName", is("第三条事件")))
+                .andExpect(jsonPath("$[2].keyword", is("分类三")))
+                .andExpect(jsonPath("$[3].eventName", is("第四条事件")))
+                .andExpect(jsonPath("$[3].keyword", is("分类四")))
                 .andExpect(status().isOk());
     }
 
