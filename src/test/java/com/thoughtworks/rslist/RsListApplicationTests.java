55106d561c4d07446de36d81aba6e7a06fb850fd
duyuqian <809009017@qq.com>
2020-08-04 19:41:01 +0800

    [duyuqian] refactor: refactor the getOneRsEvent test

--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,16 +1,15 @@
 package com.thoughtworks.rslist;
 
-import com.thoughtworks.rslist.api.RsEvent;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,8 +25,16 @@ class RsListApplicationTests {
     @Test
     void shouldGetOneRsEvent() throws Exception {
         mockMvc.perform(get("/rs/list/1"))
-                .andExpect(jsonPath("$.eventName",is("第一条事件")))
-                .andExpect(jsonPath("$.keyword",is("分类一")))
+                .andExpect(jsonPath("$.eventName", is("第一条事件")))
+                .andExpect(jsonPath("$.keyword", is("分类一")))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list/2"))
+                .andExpect(jsonPath("$.eventName", is("第二条事件")))
+                .andExpect(jsonPath("$.keyword", is("分类二")))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list/3"))
+                .andExpect(jsonPath("$.eventName", is("第三条事件")))
+                .andExpect(jsonPath("$.keyword", is("分类三")))
                 .andExpect(status().isOk());
     }
 
