35d5158e754edc3f1eab12f62493816fa1634787
duyuqian <809009017@qq.com>
2020-08-04 19:39:47 +0800

    [duyuqian] feat: pass the getOneRsEvent test

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,26 @@
 package com.thoughtworks.rslist.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+    private final List<RsEvent> rsList = init();
+
+    private List<RsEvent> init() {
+        List<RsEvent> rsEvents = new ArrayList<>();
+        rsEvents.add(new RsEvent("第一条事件", "分类一"));
+        rsEvents.add(new RsEvent("第二条事件", "分类二"));
+        rsEvents.add(new RsEvent("第三条事件", "分类三"));
+        return rsEvents;
+    }
+
+    @GetMapping("/rs/list/{index}")
+    public RsEvent getOneRsEvent(@PathVariable int index) {
+        return rsList.get(index - 1);
+    }
 }
