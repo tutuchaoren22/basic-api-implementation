1292c4f3dace03751f2e386f6785829841a76763
duyuqian <809009017@qq.com>
2020-08-06 00:22:20 +0800

    [duyuqian] feat: add the log info

--- src/main/java/com/thoughtworks/rslist/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/exception/GlobalExceptionHandler.java
@@ -1,12 +1,18 @@
 package com.thoughtworks.rslist.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 
 @ControllerAdvice
+@Slf4j
+@RestController
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({InvalidIndexException.class, InvalidParamException.class, MethodArgumentNotValidException.class})
@@ -14,13 +20,16 @@ public class GlobalExceptionHandler {
 
         String errorMessage;
         CommonError commonError = new CommonError();
+        Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
         if (e instanceof MethodArgumentNotValidException) {
             errorMessage = "invalid param";
         } else {
             errorMessage = e.getMessage();
         }
+
         commonError.setError(errorMessage);
+        logger.error("ERROR IS {} : {}", e.getClass(), e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(commonError);
     }
 }
